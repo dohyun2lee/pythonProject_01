3e1d9806f9574e3aeb646585e20d0ffec7363e2d
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-10-04 13:26:50 +0900

    Add files via upload

---
+++ transformer_test_04.py
@@ -0,0 +1,15 @@
+import copy
+import torch.nn as nn
+
+class MultiHeadAttentionLayer(nn.Module):
+
+    def __init__(self, d_model, h, qkv_fc, out_fc):
+        super(MultiHeadAttentionLayer, self).__init__()
+        self.d_model = d_model
+        self.h = h
+        self.q_fc = copy.deepcopy(qkv_fc) # (d_embed, d_model)
+        self.k_fc = copy.deepcopy(qkv_fc) # (d_embed, d_model)
+        self.v_fc = copy.deepcopy(qkv_fc) # (d_embed, d_model)
+        self.out_fc = out_fc              # (d_model, d_embed)
+
+        ...
