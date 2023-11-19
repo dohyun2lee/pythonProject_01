c88d0a37ee96b6faa58eea10fc5933937215131d
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-11-20 00:41:20 +0900

    Add files via upload

---
+++ transformer_test_14.py
@@ -0,0 +1,13 @@
+class EncoderBlock(nn.Module):
+
+    def __init__(self, self_attention, position_ff):
+        super(EncoderBlock, self).__init__()
+        self.self_attention = self_attention
+        self.position_ff = position_ff
+        self.residuals = [ResidualConnectionLayer() for _ in range(2)]
+
+    def forward(self, src, src_mask):
+        out = src
+        out = self.residuals[0](out, lambda out: self.self_attention(query=out, key=out, value=out, mask=src_mask))
+        out = self.residuals[1](out, self.position_ff)
+        return out
